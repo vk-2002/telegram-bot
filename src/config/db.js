45d4418d61ebd5adc93ae8fc90c0f9d15cd62b5c
Siddhesh Shende <138310226+siddheshshende@users.noreply.github.com>
2024-07-19 21:13:14 +0530

    made some changes in code and solved some bugs

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,5 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-export default async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_CONNECT_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      ssl: true,
-    });
-    console.log('MongoDb database connected successfully');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1);
-  }
+export default()=> {
+ return mongoose.connect(process.env.MONGO_CONNECT_STRING);
 };
