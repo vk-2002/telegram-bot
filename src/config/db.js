ad4b4a07d2c22c4295bc001f66fd2b149fe90e5a
Siddhesh Shende <138310226+siddheshshende@users.noreply.github.com>
2024-07-20 00:42:24 +0530

    changes scripts field

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 export default async () => {
   try {
-    await mongoose.connect(process.env.MONGO_CONNECT_STRING);
+    await mongoose.connect(process.env.MONGO_CONNECT_STRING, {
+      ssl: true,
+      sslValidate: false,
+      tls: true,
+      tlsInsecure: true,
+      tlsAllowInvalidCertificates: true,
+    });
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
